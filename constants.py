2aed7383721f3ec1c76e4f3f3b3566bc3b72b7e2
EyalHadad <eyalhadad1989@gmail>
2021-08-14 20:17:53 +0300

    first empty project commit

---
+++ constants.py
@@ -0,0 +1,16 @@
+
+
+EXTERNAL_PATH = "data/external"
+RAW_PATH = "data/raw"
+EXTERNAL_TRAIN_PATH = "data/external/train"
+EXTERNAL_TEST_PATH = "data/external/test"
+INTERIM_PATH = "data/interim"
+PROCESSED_PATH = "data/processed"
+PROCESSED_TRAIN_PATH = "data/processed/train"
+PROCESSED_EVALUATION_PATH = "data/processed/evaluation"
+MODELS_OBJECTS_PATH = "models/objects"
+MODELS_OUTPUT_PATH = "models/learning_output"
+MODELS_PREDICTION_PATH = "models/prediction"
+
+DATABASE_USERNAME = "drugsmaster"
+DATABASE_PASSWORD = "pass2DRUGS!"

e59d55503e56706dcb4a772da3e649e02c5c2ca2
Eyal <eyalhadad1989@gmail.com>
2021-08-30 17:00:07 +0300

    initial model class

--- src/models/training/Base_trainer.py
+++ src/models/training/Base_trainer.py
@@ -3,44 +3,41 @@ import os
 from constants import *
 from time import gmtime, strftime
 from src.models.model_learner import ModelLearner
-from src.models.vanilla_models_configurations import base_learning, rnn_learning,miTrans_base_model
+from src.models.vanilla_models_configurations import base_learning,cnn_learning2,rnn_learning, miTrans_base_model
 import numpy as np
 import pandas as pd
+import logging
+logging.getLogger("tensorflow").setLevel(logging.CRITICAL)
 
 from tensorflow.python.keras.models import save_model
 
+
 class BaseTrainObj(ModelLearner):
     history = None
-    def __init__(self,org_name,m_name):
-        ModelLearner.__init__(self,org_name,m_name)
 
+    def __init__(self, org_name, m_name):
+        ModelLearner.__init__(self, org_name, m_name)
 
     def train_model(self):
         super().prep_model_training()
-        print("---Start training {0} on {1}---\n".format(self.model_name,self.org_name))
-
-
+        print("---Start training {0} on {1}---\n".format(self.model_name, self.org_name))
 
         self.model = miTrans_base_model(self.x.shape[1])
-        self.history = self.model.fit([self.x,self.sequences], self.y, epochs=5,
-                                      validation_data=([self.xval,self.sequences_tst], self.yval))
+        self.history = self.model.fit([self.x, self.sequences], self.y, epochs=5,
+                                      validation_data=([self.xval, self.sequences_tst], self.yval))
 
-
-        # self.model = cnn_learning()
+        # self.model = cnn_learning2()
         # self.model = rnn_learning()
         # self.history = self.model.fit(self.sequences, self.y, batch_size=256, epochs=5, verbose=2, validation_data=(self.sequences_tst, self.yval))
 
         print("---Learning Curves---\n")
         self.plot_learning_curves()
         model_name = os.path.join(MODELS_OBJECTS_PATH,
-                                  '{0}_{1}_{2}'.format(self.model_name,self.org_name, strftime("%Y-%m-%d", gmtime())))
-        save_model(self.model,model_name)
+                                  '{0}_{1}_{2}'.format(self.model_name, self.org_name, strftime("%Y-%m-%d", gmtime())))
+        save_model(self.model, model_name)
         print("---{0} model saved---\n".format(self.model_name))
 
-
-
     def plot_learning_curves(self):
-
         acc = self.history.history['acc']
         val_acc = self.history.history['val_acc']
         loss = self.history.history['loss']
@@ -61,12 +58,10 @@ class BaseTrainObj(ModelLearner):
         plt.savefig(os.path.join(MODELS_OUTPUT_PATH, 'Base {0} Loss.png'.format(self.org_name)))
         plt.clf()
 
-
     def model_explain(self):
         print("---Explain model---\n")
         self.feature_importance()
         super().model_explain()
-
 
     def feature_importance(self):
         print("feature_importances\n")
@@ -78,4 +73,3 @@ class BaseTrainObj(ModelLearner):
         plt.title(title)
         plt.savefig(os.path.join(MODELS_FEATURE_IMPORTANCE, '{0}.png'.format(title)))
         plt.clf()
-
--- src/models/vanilla_models_configurations.py
+++ src/models/vanilla_models_configurations.py
@@ -1,10 +1,12 @@
-from keras.layers import Dense,Dropout,Conv1D,GlobalMaxPooling1D,Embedding,MaxPooling1D,Flatten,LSTM,Input,concatenate
-from keras.models import Sequential,Model
+from abc import ABC
+
+from keras.layers import Dense, Dropout, Conv1D, GlobalMaxPooling1D, Embedding, MaxPooling1D, Flatten, LSTM, Input, \
+    concatenate
+from keras.models import Sequential, Model
 from keras.constraints import maxnorm
 from keras.optimizers import Adam
 from keras.regularizers import l1
-
-
+from keras import layers, Model
 
 
 def base_learning(my_shape):
@@ -12,14 +14,15 @@ def base_learning(my_shape):
     model = Sequential()
     model.add(Dense(my_shape, input_dim=my_shape, activation='relu'))
     model.add(Dropout(rate=0.5))
-    model.add(Dense(my_shape/2, activation='relu',kernel_constraint=maxnorm(3),activity_regularizer=l1(0.001),kernel_regularizer=l1(0.001)))
+    model.add(Dense(my_shape / 2, activation='relu', kernel_constraint=maxnorm(3), activity_regularizer=l1(0.001),
+                    kernel_regularizer=l1(0.001)))
     model.add(Dropout(rate=0.5))
-    model.add(Dense(my_shape/4, activation='relu',kernel_constraint=maxnorm(3),activity_regularizer=l1(0.001),kernel_regularizer=l1(0.001)))
-    model.add(Dense(my_shape/8, activation='relu'))
+    model.add(Dense(my_shape / 4, activation='relu', kernel_constraint=maxnorm(3), activity_regularizer=l1(0.001),
+                    kernel_regularizer=l1(0.001)))
+    model.add(Dense(my_shape / 8, activation='relu'))
     model.add(Dense(1, activation='sigmoid'))
     model.compile(loss='binary_crossentropy', optimizer="Adam", metrics=['acc'])
     return model
-
 
 
 def cnn_learning2():
@@ -39,12 +42,12 @@ def cnn_learning2():
     return model
 
 
-
 def rnn_learning():
     # define model
     model = Sequential()
     model.add(Embedding(5, 100, input_length=130))
-    model.add(LSTM(100))
+    model.add(Conv1D(filters=32, kernel_size=8, activation='relu'))
+    # model.add(LSTM(100))
     model.add(MaxPooling1D(pool_size=2))
     model.add(Flatten())
     model.add(Dense(10, activation='relu'))
@@ -76,7 +79,7 @@ def miTrans_base_model(my_shape):
     cnn_branch = build_cnn_branch()
     combined = concatenate([ann_branch.output, cnn_branch.output])
     z = Dense(2, activation="relu")(combined)
-    z = Dense(1, activation = 'sigmoid')(z)
+    z = Dense(1, activation='sigmoid')(z)
     model = Model(inputs=[ann_branch.input, cnn_branch.input], outputs=z)
     # opt = Adam(lr=1e-3, decay=1e-3 / 200)
     # model.compile(loss="mean_absolute_percentage_error", optimizer=opt)
@@ -84,6 +87,77 @@ def miTrans_base_model(my_shape):
     model.compile(loss='binary_crossentropy', optimizer="Adam", metrics=['acc'])
     print(model.summary())
     return model
+
+
+class miTransfer(Model):
+    def __init__(self, input_shape, vocab_size=5, maxlen=130, embed_dim=100):
+        super(miTransfer, self).__init__()
+
+        self.cnn_input = Input(shape=(maxlen,))
+        self.cnn_embeddings = Embedding(vocab_size, embed_dim)
+        self.cnn_conv1 = Conv1D(filters=32, kernel_size=8, activation='relu')
+        self.cnn_maxpool = MaxPooling1D(pool_size=2)
+        self.cnn_flatten = Flatten()
+        self.cnn_dense = Dense(10, activation='relu')
+
+        y = MaxPooling1D(pool_size=2)(y)
+        y = Flatten()(y)
+        y = Dense(10, activation='relu')(y)
+        y = Dense(1, activation='sigmoid')(y)
+
+        inputs = Input(shape=(130,))
+        y = Embedding(5, 100)(inputs)
+        y = Conv1D(filters=32, kernel_size=8, activation='relu')(y)
+        y = MaxPooling1D(pool_size=2)(y)
+        y = Flatten()(y)
+        y = Dense(10, activation='relu')(y)
+        y = Dense(1, activation='sigmoid')(y)
+        y = Model(inputs, y)
+    #     ann_branch = self.build_ann_branch(input_shape)
+    # cnn_branch = build_cnn_branch()
+
+    # super(NERModel, self).__init__()
+    # self.embedding_layer = TokenAndPositionEmbedding(maxlen, vocab_size, embed_dim)
+    # self.transformer_block = TransformerBlock(embed_dim, num_heads, ff_dim)
+    # self.dropout1 = layers.Dropout(0.1)
+    # self.ff = layers.Dense(ff_dim, activation="relu")
+    # self.dropout2 = layers.Dropout(0.1)
+    # self.ff_final = layers.Dense(num_tags, activation="softmax")
+
+    def call(self, inputs, training=False):
+        x = self.embedding_layer(inputs)
+        x = self.transformer_block(x)
+        x = self.dropout1(x, training=training)
+        x = self.ff(x)
+        x = self.dropout2(x, training=training)
+        x = self.ff_final(x)
+        return x
+
+    def build_ann_branch(my_shape):
+        inputs = Input(shape=(my_shape,))
+        x = Dense(my_shape, activation='relu')(inputs)
+        x = Dense(my_shape / 2, activation='relu', kernel_constraint=maxnorm(3), activity_regularizer=l1(0.001),
+                  kernel_regularizer=l1(0.001))(x)
+        x = Dropout(rate=0.5)(x)
+        x = Dense(my_shape / 4, activation='relu', kernel_constraint=maxnorm(3), activity_regularizer=l1(0.001),
+                  kernel_regularizer=l1(0.001))(x)
+        x = Dropout(rate=0.5)(x)
+        x = Dense(my_shape / 8, activation='relu', kernel_constraint=maxnorm(3), activity_regularizer=l1(0.001),
+                  kernel_regularizer=l1(0.001))(x)
+        x = Dense(1, activation='sigmoid')(x)
+        x = Model(inputs, x)
+        return x
+
+    def build_cnn_branch():
+        inputs = Input(shape=(130,))
+        y = Embedding(5, 100)(inputs)
+        y = Conv1D(filters=32, kernel_size=8, activation='relu')(y)
+        y = MaxPooling1D(pool_size=2)(y)
+        y = Flatten()(y)
+        y = Dense(10, activation='relu')(y)
+        y = Dense(1, activation='sigmoid')(y)
+        y = Model(inputs, y)
+        return y
 
 
 def build_ann_branch(my_shape):
@@ -100,5 +174,3 @@ def build_ann_branch(my_shape):
     x = Dense(1, activation='sigmoid')(x)
     x = Model(inputs, x)
     return x
-
-

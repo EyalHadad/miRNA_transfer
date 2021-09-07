5024eb73783df98af169430f6b978242158a0f17
EyalHadad <eyalhadad1989@gmail>
2021-09-07 18:20:45 +0300

    general dataset list

--- notebooks/training.py
+++ notebooks/training.py
@@ -3,22 +3,20 @@ from src.models.training.GaussianNB_trainer import NbTrainObj
 from src.models.training.RF_trainer import RfTrainObj
 from src.models.training.Base_trainer import BaseTrainObj
 
-
 if __name__ == '__main__':
     # dataset_list = ['cow', 'human1', 'human2', 'human3', 'worm1', 'worm2', 'mouse1', 'mouse2']
     dataset_list = ['worm1']
     for model_type in [4]:
         for data in dataset_list:
-            if model_type==1:
-                train_obj = XgboostTrainObj(data,'Xgboost')
-            elif model_type==2:
-                train_obj = NbTrainObj(data,'GaussianNB')
+            if model_type == 1:
+                train_obj = XgboostTrainObj(data, 'Xgboost')
+            elif model_type == 2:
+                train_obj = NbTrainObj(data, 'GaussianNB')
             elif model_type == 3:
                 train_obj = RfTrainObj(data, 'Random forest')
             else:
-                train_obj = BaseTrainObj(data,'Base')
+                train_obj = BaseTrainObj(data, 'Base')
 
             train_obj.train_model()
             # train_obj.model_explain()
             train_obj.evaluate_model()
-
--- notebooks/transferring.py
+++ notebooks/transferring.py
@@ -1,18 +1,15 @@
 from src.models.transfer.Transfer_obj import Transfer_obj
 from constants import *
 
-
 if __name__ == '__main__':
     # dataset_list = DATASETS
     dataset_list = ['worm1']
-    rest = DATASETS
-    rest.remove('worm1')
-    rest = ['mouse1', 'mouse2']
-    transfer_size = [0,100,200,500,800]
+    transfer_size = [0, 100, 200, 500, 800]
     for org_name in dataset_list:
+        rest = DATASETS
+        rest.remove(org_name)
         trans_obj = Transfer_obj(org_name)
         for dst_org_name in rest:
             trans_obj.load_dst_data(dst_org_name)
             for t_size in transfer_size:
                 trans_obj.retrain_model(t_size)
-
